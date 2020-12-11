a7388306e6c0d9b8b8eb7bc2a24b14af94e24689
Mohamed Saleh <msaleh@evero.net>
2020-12-11 13:41:23 -0500

    formatting

--- src/App.vue
+++ src/App.vue
@@ -1,61 +1,65 @@
 <template>
   <div class="container">
+
     <SearchBar @termChange="onTermChange"></SearchBar>
     <div class="row">
-      <VideoDetail v-bind:video="selectedVideo"></VideoDetail>
-      <VideoList @videoSelect="onVideoSelect" :videos="videos" />
+ <VideoDetail v-bind:video="selectedVideo"></VideoDetail> 
+    <VideoList @videoSelect="onVideoSelect" :videos="videos"/>
     </div>
-
+   
+    
     <!--
       <VideoList v-bind:videos="videos"/>
       <VideoList :videos="videos"/>
     -->
-    <div>Total number of videos :: {{ videos.length }}</div>
+    <div>
+      Total number of videos :: {{videos.length}}
+    </div>
   </div>
+ 
 </template>
 <script>
-import SearchBar from "./components/SearchBar.vue";
-import VideoList from "./components/VideoList.vue";
-import VideoDetail from "./components/VideoDetails.vue";
+import SearchBar from './components/SearchBar.vue'
+import VideoList from './components/VideoList.vue'
+import VideoDetail from './components/VideoDetails.vue';
 /**/
-import axios from "axios";
-const API_KEY = "AIzaSyCn1wKF5XAP4rjNN_vAarfCb3HmabBWfCI";
+import axios from 'axios'
+const API_KEY ='AIzaSyCn1wKF5XAP4rjNN_vAarfCb3HmabBWfCI'
+
 
 export default {
-  name: "app",
+  name: 'app',
   components: {
     SearchBar,
-    VideoList,
-    VideoDetail,
+    VideoList, 
+    VideoDetail
   },
-  data() {
+  data () {
     return {
-      videos: [],
-      selectedVideo: null,
-    };
+      videos:[],
+      selectedVideo: null
+    }
   },
   methods: {
-    onVideoSelect: function (video) {
+    onVideoSelect: function(video){
       this.selectedVideo = video;
-      console.log("Video Selected Updated.");
+      console.log('Video Selected Updated.');
     },
-    onTermChange: function (searchTerm) {
-      console.log(searchTerm);
-      /**/
-      /**/
-      axios
-        .get("https://www.googleapis.com/youtube/v3/search", {
-          params: {
-            key: API_KEY,
-            type: "video",
-            part: "snippet",
-            q: searchTerm,
-          },
-        })
-        .then((response) => {
-          this.videos = response.data.items;
-        });
-    },
-  },
-};
+    onTermChange: function(searchTerm) {
+        console.log(searchTerm);
+        /**/
+        /**/
+        axios.get('https://www.googleapis.com/youtube/v3/search',{
+          params:{
+            key:API_KEY,
+            type:'video',
+            part:'snippet',
+            q: searchTerm
+          }
+       }).then(response => {this.videos = response.data.items;});
+       
+       
+    }
+  }
+}
 </script>
